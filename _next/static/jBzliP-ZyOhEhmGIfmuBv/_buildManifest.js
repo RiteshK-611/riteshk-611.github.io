9547a2eef4dd393c394c7c571c78efaef5c58bfe
RiteshK-611 <61982298+RiteshK-611@users.noreply.github.com>
2022-06-16 19:19:11 +0000

    Deploying to gh-pages from @ RiteshK-611/riteshk-611.github.io@9ab9efc4d38d14869ed6f490221afcd2860a11e8 🚀

---
+++ _next/static/jBzliP-ZyOhEhmGIfmuBv/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/d0447323-066c9cafb93145df.js","static/chunks/252f366e-841516e568a64306.js","static/chunks/pages/index-653bf5845451874f.js"],"/_error":["static/chunks/pages/_error-0a004b8b8498208d.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
