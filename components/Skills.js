88c1754106f3c8b69c7a84962237e2403a1f2a44
Ritesh <riteshkokam@gmail.com>
2024-04-14 21:41:48 +0530

    attempt - update chatpdf image

--- components/Skills.js
+++ components/Skills.js
@@ -1,22 +1,4 @@
 import {
-  SiMongodb,
-  SiFirebase,
-  SiTailwindcss,
-  SiMui,
-  SiNextdotjs,
-  SiHeroku,
-  SiTypescript,
-  SiJavascript,
-  SiReact,
-  SiHtml5,
-  SiCss3,
-  SiGit,
-  SiGithub,
-  SiNodedotjs,
-  SiFigma,
-  SiNpm,
-} from "react-icons/si";
-import {
   FaHtml5,
   FaCss3Alt,
   FaJsSquare,
@@ -27,31 +9,20 @@ import {
   FaGithub,
   FaFigma,
 } from "react-icons/fa";
+import {
+  SiMongodb,
+  SiFirebase,
+  SiTailwindcss,
+  SiMui,
+  SiNextdotjs,
+  SiHeroku,
+  SiTypescript,
+} from "react-icons/si";
 
 const Skills = () => {
-  const grp = `group flex flex-col md:flex-row justify-center items-center hover:justify-evenly`;
-  const icon = `text-[#555555] opacity-60 group-hover:fill-blue-500 group-hover:opacity-100 ease-out duration-300`;
+  const grp = `group py-5 flex flex-col md:flex-row justify-center items-center hover:justify-evenly dark:neumorphism-dark neumorphism-light relative overflow-hidden`;
+  const icon = `text-[#555555] opacity-60 group-hover:fill-blue-500 group-hover:opacity-100 ease-in-out duration-10000 w-12 h-12 sm:w-13 sm:h-13`;
   const name = `invisible md:hidden group-hover:visible group-hover:md:block ease-in-out duration-200`;
-
-  const skills = [
-    { icon: <SiHtml5 className={icon} />, name: "HTML" },
-    { icon: <SiCss3 className={icon} />, name: "CSS" },
-    { icon: <SiJavascript className={icon} />, name: "Javascript" },
-    { icon: <SiReact className={icon} />, name: "React" },
-    { icon: <SiNodedotjs className={icon} />, name: "Node" },
-    { icon: <SiMongodb className={icon} />, name: "MongoDB" },
-    { icon: <SiFirebase className={icon} />, name: "Firebase" },
-    { icon: <SiTailwindcss className={icon} />, name: "Tailwind" },
-    { icon: <SiMui className={icon} />, name: "MaterialUI" },
-    { icon: <SiNpm className={icon} />, name: "NPM" },
-    { icon: <SiGit className={icon} />, name: "GIT" },
-    { icon: <SiGithub className={icon} />, name: "Github" },
-    { icon: <SiFigma className={icon} />, name: "Figma" },
-    { icon: <SiNextdotjs className={icon} />, name: "Next" },
-    { icon: <SiHeroku className={icon} />, name: "Heroku" },
-    { icon: <SiTypescript className={icon} />, name: "Typescript" },
-  ];
-
   return (
     <div id="skills" className="w-full md:h-screen p-2 py-16">
       <div className="max-w-[1240px] m-auto flex flex-col justify-center h-full">
