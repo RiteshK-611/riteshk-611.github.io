96729e747fef6f9c69643775bdf214db366ac592
Ritesh <riteshkokam@gmail.com>
2024-05-19 11:25:37 +0530

    Revert "format projectitem.js"
    This reverts commit db1fdde2c1e3646914918e1828cf29af02b46439.

--- components/ProjectItem.js
+++ components/ProjectItem.js
@@ -2,31 +2,24 @@ import React from "react";
 import Image from "next/image";
 import Link from "next/link";
 
-const ProjectItem = ({
-  bgImg,
-  title,
-  description,
-  projectDemo,
-  projectCode,
-  usedArray,
-}) => {
+const ProjectItem = ({ bgImg, title , description, projectDemo, projectCode, usedArray }) => {
   return (
     <div className="md:grid grid-cols-3 gap-8 m-auto p-5 group">
-      <div>
+      <div className="col-span-1 relative flex items-center justify-center w-full h-auto m-auto shadow-xl shadow-gray-400 dark:shadow-2xl dark:shadow-black bg-gray-800 rounded-xl p-1 group-hover:shadow-none ease-in duration-300">
         <Image className="rounded-lg" src={bgImg} alt="/" />
       </div>
       <div className="col-span-2 m-auto pt-5 md:pt-0 p-1 ">
         <p className="pb-4 text-xl font-bold group-hover:underline-offset-4 group-hover:underline ease-in duration-300">
           {title}
         </p>
-        <p>{description}</p>
+        <p>
+          {description}
+        </p>
         <div className="pt-3">
           <p className="text-sm font-semibold pb-1">Technologies used:</p>
           {usedArray.map((used, i) => {
             return (
-              <span
-                key={i}
-                className="rounded-[3px] text-xs font-bold text-[#0967d2] border border-[#0967d2] border-sky-500 m-[3px] first-of-type:ml-0 py-1 px-3 hover:bg-blue-500 hover:text-white inline-block ease-in duration-300">
+              <span key={i} className="rounded-[3px] text-xs font-bold text-[#0967d2] border border-[#0967d2] border-sky-500 m-[3px] first-of-type:ml-0 py-1 px-3 hover:bg-blue-500 hover:text-white inline-block ease-in duration-300">
                 {used}
               </span>
             );
