38e4eda99b4a7a1a362bd366b3025ce75c343eb5
Ritesh <riteshkokam@gmail.com>
2024-05-19 11:26:15 +0530

    Revert "update skills.js"
    This reverts commit aab9dc058e3315f2798153fce680269d2f4050ee.

--- components/Skills.js
+++ components/Skills.js
@@ -18,28 +18,9 @@ import {
 } from "react-icons/si";
 
 const Skills = () => {
-  const grp = `group py-5 flex flex-col md:flex-row justify-center items-center hover:justify-evenly dark:neumorphism-dark neumorphism-light relative overflow-hidden`;
-  const icon = `text-[#555555] opacity-60 group-hover:fill-blue-500 group-hover:opacity-100 ease-in-out duration-10000 w-12 h-12 sm:w-13 sm:h-13`;
+  const grp = `group py-5 flex flex-col md:flex-row justify-center items-center hover:justify-evenly dark:neumorphism-dark neumorphism-light`;
+  const icon = `text-[#555555] opacity-60 group-hover:fill-blue-500 group-hover:opacity-100 ease-out duration-300 w-12 h-12 sm:w-13 sm:h-13`;
   const name = `invisible md:hidden group-hover:visible group-hover:md:block ease-in-out duration-200`;
-
-  const skills = [
-    { icon: <SiHtml5 className={icon} />, name: "HTML" },
-    { icon: <SiCss3 className={icon} />, name: "CSS" },
-    { icon: <SiJavascript className={icon} />, name: "Javascript" },
-    { icon: <SiReact className={icon} />, name: "React" },
-    { icon: <SiNodedotjs className={icon} />, name: "Node" },
-    { icon: <SiMongodb className={icon} />, name: "MongoDB" },
-    { icon: <SiFirebase className={icon} />, name: "Firebase" },
-    { icon: <SiTailwindcss className={icon} />, name: "Tailwind" },
-    { icon: <SiMui className={icon} />, name: "MaterialUI" },
-    { icon: <SiNpm className={icon} />, name: "NPM" },
-    { icon: <SiGit className={icon} />, name: "GIT" },
-    { icon: <SiGithub className={icon} />, name: "Github" },
-    { icon: <SiFigma className={icon} />, name: "Figma" },
-    { icon: <SiNextdotjs className={icon} />, name: "Next" },
-    { icon: <SiHeroku className={icon} />, name: "Heroku" },
-    { icon: <SiTypescript className={icon} />, name: "Typescript" },
-  ];
 
   return (
     <div id="skills" className="w-full md:h-screen p-2 py-16">
@@ -49,15 +30,70 @@ const Skills = () => {
         </p>
         <h2 className="py-4">What I can do</h2>
         <div className="grid grid-cols-3 sm:grid-cols-4 gap-8">
-          {skills.map((skill, index) => (
-            <div key={index} className={grp}>
-              {/* <div className="absolute w-full h-full bg-red-500 opacity-80 z-10 transform transition-transform duration-500 ease-in-out group-hover:-translate-x-full">
-                <div className="absolute w-3 h-1/2 bg-red-900 rounded-l-xl top-1/4 right-0 z-20"></div>
-              </div> */}
-              {skill.icon}
-              <h6 className={name}>{skill.name}</h6>
-            </div>
-          ))}
+          <div className={grp}>
+            <SiHtml5 className={icon} />
+            <h6 className={name}>HTML</h6>
+          </div>
+          <div className={grp}>
+            <SiCss3 className={icon} />
+            <h6 className={name}>CSS</h6>
+          </div>
+          <div className={grp}>
+            <SiJavascript className={icon} />
+            <h6 className={name}>Javascript</h6>
+          </div>
+          <div className={grp}>
+            <SiReact className={icon} />
+            <h6 className={name}>React</h6>
+          </div>
+          <div className={grp}>
+            <SiNodedotjs className={icon} />
+            <h6 className={name}>Node</h6>
+          </div>
+          <div className={grp}>
+            <SiMongodb className={icon} />
+            <h6 className={name}>MongoDB</h6>
+          </div>
+          <div className={grp}>
+            <SiFirebase className={icon} />
+            <h6 className={name}>Firebase</h6>
+          </div>
+          <div className={grp}>
+            <SiTailwindcss className={icon} />
+            <h6 className={name}>Tailwind</h6>
+          </div>
+          <div className={grp}>
+            <SiMui className={icon} />
+            <h6 className={name}>MaterialUI</h6>
+          </div>
+          <div className={grp}>
+            <SiNpm className={icon} />
+            <h6 className={name}>NPM</h6>
+          </div>
+          <div className={grp}>
+            <SiGit className={icon} />
+            <h6 className={name}>GIT</h6>
+          </div>
+          <div className={grp}>
+            <SiGithub className={icon} />
+            <h6 className={name}>Github</h6>
+          </div>
+          <div className={grp}>
+            <SiFigma className={icon} />
+            <h6 className={name}>Figma</h6>
+          </div>
+          <div className={grp}>
+            <SiNextdotjs className={icon} />
+            <h6 className={name}>Next</h6>
+          </div>
+          <div className={grp}>
+            <SiHeroku className={icon} />
+            <h6 className={name}>Heroku</h6>
+          </div>
+          <div className={grp}>
+            <SiTypescript className={icon} />
+            <h6 className={name}>Typescript</h6>
+          </div>
         </div>
       </div>
     </div>
